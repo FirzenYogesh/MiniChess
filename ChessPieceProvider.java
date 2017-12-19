5b265a4cc0ff8c93408250049fd00e6ebad321cf
Yogesh S <contact@syogesh.in>
2017-12-19 16:38:46 +0530

    Major Updates: Player can choose the piece to play. There is a piece limit.
    Minor Updates:

---
+++ ChessPieceProvider.java
@@ -0,0 +1,35 @@
+public class ChessPieceProvider {
+    int numberOfPawns = 3;
+    int numberOfCastle = 1;
+    int numberOfBishop = 1;
+
+    static ChessPieceProvider provideChessPieces(int numberOfPieces) {
+        ChessPieceProvider provider = new ChessPieceProvider();
+        for (int i = 6; i <= numberOfPieces; i++) {
+            if (i % 3 == 0)
+                provider.numberOfCastle++;
+            else if (i % 3 == 1)
+                provider.numberOfBishop++;
+            else
+                provider.numberOfPawns++;
+        }
+        return provider;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Pawns: %d\t Castle: %d\t Bishop: %d", numberOfPawns, numberOfCastle, numberOfBishop);
+    }
+
+    public int getNumberOfBishop() {
+        return numberOfBishop;
+    }
+
+    public int getNumberOfCastle() {
+        return numberOfCastle;
+    }
+
+    public int getNumberOfPawns() {
+        return numberOfPawns;
+    }
+}
--- Play.java
+++ Play.java
@@ -7,9 +7,11 @@ public class Play {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int numberOfPieces;
-        System.out.println("Enter the number of pieces");
-        numberOfPieces = scanner.nextInt();
+        int numberOfPieces = 0;
+        while (numberOfPieces < 5) {
+            System.out.println("Enter the number of pieces (min 5): ");
+            numberOfPieces = scanner.nextInt();
+        }
         ChessPiece[] player1Pieces = new ChessPiece[numberOfPieces];
         ChessPiece[] player2Pieces = new ChessPiece[numberOfPieces];
         ChessPieceUtils.player1PiecesLeft = numberOfPieces;
@@ -17,28 +19,45 @@ public class Play {
         scanner.nextLine();
         System.out.println();
         System.out.println("Enter Player 1 (P1) Pieces");
-        ArrayList<String> tempList = new ArrayList<>();
+        player1Pieces = getPlayerPieces(numberOfPieces, ChessPieceUtils.player1);
+        /*ArrayList<String> tempList = new ArrayList<>();
+        ChessPieceProvider provider = ChessPieceProvider.provideChessPieces(numberOfPieces);
         for (int i = 0; i < numberOfPieces; i++) {
+            System.out.println("Enter the Piece Type:\np for Pawn\nc for Castle\nb for Bishop\n");
+            String pieceType = scanner.nextLine().trim();
+            while (!pieceType.toLowerCase().equals("p") || !pieceType.toLowerCase().equals("c") || !pieceType.toLowerCase().equals("b")) {
+                System.out.println("Enter a valid Piece Type");
+                pieceType = scanner.nextLine().trim();
+            }
+            switch (pieceType.toLowerCase()) {
+                case "p":
+                    provider.numberOfPawns--;
+                    break;
+                case "c":
+                    provider.numberOfBishop--;
+                    break;
+                case "b":
+                    provider.numberOfCastle--;
+                    break;
+            }
             System.out.println("Enter the name of the piece");
-            String name = scanner.nextLine();
-            name = name.trim();
+            String name = scanner.nextLine().trim();
             while (name.equals("") || name.equals("\n")) {
                 System.out.println("Please enter a valid Character name");
-                name = scanner.nextLine();
-                name = name.trim();
+                name = scanner.nextLine().trim();
             }
             while (tempList.contains(name)) {
                 System.out.println("Please enter a Unique Character name");
-                name = scanner.nextLine();
-                name = name.trim();
+                name = scanner.nextLine().trim();
             }
             tempList.add(name);
             player1Pieces[i] = new PieceCastle(name, ChessPieceUtils.player1, new Position(numberOfPieces - 1, i));
         }
-        tempList = new ArrayList<>();
+        tempList = new ArrayList<>();*/
         System.out.println();
         System.out.println("Enter Player 2 (P2) Pieces");
-        for (int i = numberOfPieces - 1; i >= 0; i--) {
+        player2Pieces = getPlayerPieces(numberOfPieces, ChessPieceUtils.player2);
+        /*for (int i = numberOfPieces - 1; i >= 0; i--) {
             System.out.println("Enter the name of the piece");
             String name = scanner.nextLine();
             name = name.trim();
@@ -54,7 +73,7 @@ public class Play {
             }
             tempList.add(name);
             player2Pieces[i] = new PieceCastle(name, ChessPieceUtils.player2, new Position(0, i));
-        }
+        }*/
         ChessBoard chessBoard = new ChessBoard(player1Pieces, player2Pieces, numberOfPieces, numberOfPieces);
         while (ChessPieceUtils.player1PiecesLeft > 0 && ChessPieceUtils.player2PiecesLeft > 0) {
             System.out.println();
@@ -76,5 +95,91 @@ public class Play {
         } else {
             System.out.println("Player 1 Wins");
         }
+    }
+
+    private static ChessPiece[] getPlayerPieces(int numberOfPieces, String player) {
+        Scanner scanner = new Scanner(System.in);
+        ChessPiece[] pieces = new ChessPiece[numberOfPieces];
+        ArrayList<String> tempList = new ArrayList<>();
+        ChessPieceProvider provider = ChessPieceProvider.provideChessPieces(numberOfPieces);
+        int i = 0;
+        while (i < numberOfPieces) {
+            System.out.println();
+            System.out.println("Enter the Piece Type:\np for Pawn\nc for Castle\nb for Bishop\n");
+            String pieceType = scanner.nextLine().trim();
+            while (!pieceType.toLowerCase().equals("p") && !pieceType.toLowerCase().equals("c") && !pieceType.toLowerCase().equals("b")) {
+                System.out.println("Enter a valid Piece Type");
+                pieceType = scanner.nextLine().trim();
+            }
+            switch (pieceType.toLowerCase()) {
+                case "p":
+                    if (provider.numberOfPawns > 0)
+                        break;
+                    else {
+                        System.out.println();
+                        System.out.println("You have reached the limit of Pawns you can use");
+                        continue;
+                    }
+                case "c":
+                    if (provider.numberOfCastle > 0)
+
+                        break;
+                    else {
+                        System.out.println("You have reached the limit of Castle you can use");
+                        continue;
+                    }
+                case "b":
+                    if (provider.numberOfBishop > 0)
+                        break;
+                    else {
+                        System.out.println("You have reached the limit of Bishop you can use");
+                        continue;
+                    }
+            }
+            System.out.println("Enter the name of the piece");
+            String name = scanner.nextLine().trim();
+            while (name.equals("") || name.equals("\n")) {
+                System.out.println("Please enter a valid Character name");
+                name = scanner.nextLine().trim();
+            }
+            while (tempList.contains(name)) {
+                System.out.println("Please enter a Unique Character name");
+                name = scanner.nextLine().trim();
+            }
+            tempList.add(name);
+            Position position;
+            if (player.equals(ChessPieceUtils.player1))
+                position = new Position(numberOfPieces - 1, i);
+            else
+                position = new Position(0, (numberOfPieces - 1) - i);
+            ChessPiece piece;
+            switch (pieceType.toLowerCase()) {
+                case "p":
+                    provider.numberOfPawns--;
+                    piece = new PiecePawn(name, player, position);
+                    break;
+                case "c":
+                    provider.numberOfBishop--;
+                    piece = new PieceCastle(name, player, position);
+                    break;
+                case "b":
+                    provider.numberOfCastle--;
+                    piece = new PieceBishop(name, player, position);
+                    break;
+                default:
+                    piece = ChessPieceUtils.noPiece;
+            }
+            if (player.equals(ChessPieceUtils.player1)) {
+                pieces[i] = piece;
+            } else {
+                pieces[(numberOfPieces - 1) - i] = piece;
+            }
+            i++;
+        }
+        /*for (int i = 0; i < numberOfPieces; i++) {
+
+            //pieces[i] = new PieceCastle(name, ChessPieceUtils.player1, new Position(numberOfPieces - 1, i));
+        }*/
+        return pieces;
     }
 }
